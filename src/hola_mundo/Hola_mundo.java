91b5b89529d35381314e5dc7de711149782f7236
Anahí Rocha <anhai25rrz@gmail.com>
2021-02-16 17:32:41 -0600

    Esta es mi primer versión

---
+++ src/hola_mundo/Hola_mundo.java
@@ -0,0 +1,25 @@
+/*
+ *  Hola mundo
+ * Anahí Lizeth Rocha Rodríguez
+ */
+package hola_mundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Anhai
+ */
+
+public class Hola_mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+         JOptionPane.showMessageDialog(null, "Hola Mundo");
+        
+    }
+    
+}
